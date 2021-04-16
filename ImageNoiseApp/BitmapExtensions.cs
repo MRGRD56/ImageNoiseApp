61dc9ea722b4f86632d8a4d800f5ee451fdd6eb0
MRGRD56 <kirgol85@gmail.com>
2021-04-16 13:29:42 +0500

    Добавьте файлы проекта.

---
+++ ImageNoiseApp/BitmapExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageNoiseApp
+{
+    public class BitmapExtensions
+    {
+        public void AddNoise(Bitmap image, int level)
+        {
+            var random = new Random();
+            for (var x = 0; x < image.Width; x++)
+            {
+                for (var y = 0; y < image.Height; y++)
+                {
+                    var pixel = image.GetPixel(x, y);
+                    int r = pixel.R;
+                    int g = pixel.G;
+                    int b = pixel.B;
+
+                    void DeformColorPart(ref int part, int noiseLevel)
+                    {
+                        var maxDownNoice = new List<int> { part, noiseLevel }.Min();
+                        var maxUpNoice = new List<int> { 255 - part, noiseLevel }.Min();
+                        part += random.Next(-maxDownNoice, maxUpNoice);
+                    }
+
+                    void AddNoiseToColor(int noiseLevel)
+                    {
+                        if (noiseLevel is < 0 or > 255)
+                        {
+                            throw new ArgumentException("Value must be [0; 255]", nameof(noiseLevel));
+                        }
+                        DeformColorPart(ref r, noiseLevel);
+                        DeformColorPart(ref g, noiseLevel);
+                        DeformColorPart(ref b, noiseLevel);
+                    }
+
+                    AddNoiseToColor(level);
+
+                    image.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+        }
+    }
+}
---
+++ ImageNoiseApp/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ImageNoiseApp
+{
+    public class MainWindowViewModel : NotifyPropertyChanged
+    {
+        private ImageSource _resultImageSource;
+        private string _originalImageSource;
+
+        public string OriginalImageSource
+        {
+            get => _originalImageSource;
+            set
+            {
+                _originalImageSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ImageSource ResultImageSource
+        {
+            get => _resultImageSource;
+            set
+            {
+                _resultImageSource = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int NoiseLevel { get; set; } = 100;
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        public Command LoadFileCommand => new(_ =>
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files|*.png;*.jpg;*.jpeg;*.bmp|All Files|*.*";
+            var result = dialog.ShowDialog();
+            if (result != true) return;
+            OriginalImageSource = dialog.FileName;
+        });
+
+        public Command LoadScreenshotCommand => new(_ => { });
+
+        public Command GenerateCommand => new(_ => { });
+
+        public Command SaveResultCommand => new(_ => { });
+    }
+}
